9b84d2195da05e17343ebd951ef26382f63cd6b5
Sally Steuterman <sally.steuterman@gmail.com>
2023-06-22 15:45:43 -0500

    Adding chapter 3 example

---
+++ control-flow-and-collections/chapter-example/student-example/src/main/java/org/launchcode/ArrayListGradebook.java
@@ -0,0 +1,44 @@
+package org.launchcode;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListGradebook {
+    public static void main(String[] args) {
+        ArrayList<String> students = new ArrayList<>();
+        ArrayList<Double> grades = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        String newStudent;
+
+        System.out.println("Enter your students (or ENTER to finish):");
+
+        // Get student names
+        do {
+            newStudent = input.nextLine();
+
+            if (!newStudent.equals("")) {
+                students.add(newStudent);
+            }
+
+        } while(!newStudent.equals(""));
+
+        // Get student grades
+        for (String student : students) {
+            System.out.print("Grade for " + student + ": ");
+            Double grade = input.nextDouble();
+            grades.add(grade);
+        }
+
+        // Print class roster
+        System.out.println("\nClass roster:");
+        double sum = 0.0;
+
+        for (int i = 0; i < students.size(); i++) {
+            System.out.println(students.get(i) + " (" + grades.get(i) + ")");
+            sum += grades.get(i);
+        }
+
+        double avg = sum / students.size();
+        System.out.println("Average grade: " + avg);
+    }
+}
---
+++ control-flow-and-collections/chapter-example/student-example/src/main/java/org/launchcode/HashMapGradebook.java
@@ -0,0 +1,44 @@
+package org.launchcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class HashMapGradebook {
+    public static void main(String[] args) {
+        HashMap<String, Double> students = new HashMap<>();
+        Scanner input = new Scanner(System.in);
+        String newStudent;
+
+        System.out.println("Enter your students (or ENTER to finish):");
+
+        // Get student names and grades
+        do {
+
+            System.out.print("Student: ");
+            newStudent = input.nextLine();
+
+            if (!newStudent.equals("")) {
+                System.out.print("Grade: ");
+                Double newGrade = input.nextDouble();
+                students.put(newStudent, newGrade);
+
+                // Read in the newline before looping back
+                input.nextLine();
+            }
+
+        } while(!newStudent.equals(""));
+
+        // Print class roster
+        System.out.println("\nClass roster:");
+        double sum = 0.0;
+
+        for (Map.Entry<String, Double> student : students.entrySet()) {
+            System.out.println(student.getKey() + " (" + student.getValue() + ")");
+            sum += student.getValue();
+        }
+
+        double avg = sum / students.size();
+        System.out.println("Average grade: " + avg);
+    }
+}
