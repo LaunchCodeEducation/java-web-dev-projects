be7dd42dc122bd4258da5c354eaf5029dcd58eb0
Courtney Frey <courtneyb.frey@gmail.com>
2023-06-27 16:13:07 -0500

    added codebases for datatypes chapter

---
+++ datatype-examples/src/main/java/org/launchcode/HelloMethods.java
@@ -0,0 +1,10 @@
+package org.launchcode;
+
+public class HelloMethods {
+
+    public static void main(String[] args) {
+        String message = Message.getMessage("fr");
+        System.out.println(message);
+    }
+
+}
