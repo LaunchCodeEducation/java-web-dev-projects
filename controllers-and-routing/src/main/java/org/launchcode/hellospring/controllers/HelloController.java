388cff7215d5f0f079b723f5047653f3a4feedde
John Woolbright <jwoolbright23@gmail.com>
2023-06-28 13:46:22 -0500

    controllers and routing studio solution

--- controllers-and-routing/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ controllers-and-routing/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -4,46 +4,83 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@ResponseBody
-@RequestMapping("hello")
 public class HelloController {
-    //handle request at path http://localhost:8080/hello
 
-//    @GetMapping("hello")
+//    @RequestMapping(value="")
 //    @ResponseBody
-//    public String hello(){
-//        return "Hello, Spring!";
+//    public String index(@RequestParam String name) {
+//        if (name == null) {
+//            name = "World";
+//        }
+//        return "Hello " + name;
 //    }
 
-    //handle request at path //http://localhost:8080/hello/goodbye
-    @GetMapping("goodbye")
-    public String goodbye(){
-        return "Goodbye, Spring!";
+    @RequestMapping(value="hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String helloForm()  {
+
+        String html = "<form method='post'>" +
+                "<input type = 'text' name = 'name' />" +
+                "<select name = 'language'>" +
+                "<option value = 'english'>English</option>" +
+                "<option value = 'french'>French</option>" +
+                "<option value = 'italian'>Italian</option>" +
+                "<option value = 'spanish'>Spanish</option>" +
+                "<option value = 'german'>German</option>" +
+                "</select>" +
+                "<input type = 'submit' value = 'Greet Me!' />" +
+                "</form>";
+        return html;
+
+        // For a bonus mission, the options of the select need to be pulled in from a model.
+        // If a student has chosen to try this mission, they need to add a models package that contains a model for the different language options.
     }
 
-    //handle requests of the form http://localhost:8080/hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name){
-        return "Hello, " + name + "!";
+    @RequestMapping(value="hello", method = RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(@RequestParam String name, @RequestParam String language) {
+        if (name == null) {
+            name = "World";
+        }
+
+        return createMessage(name, language);
+
+        // For a bonus mission, students can change this response text to look nicer.
+        // This is subjective, but students should be modifying the HTML of the response string.
     }
 
-    //handle requests of the form http://localhost:8080/hello/LaunchCode
-    @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name){
-        return "Hello, " + name + "!";
+    public static String createMessage(String n, String l) {
+        String greeting = "";
+
+        if (l.equals("english")) {
+            greeting = "Hello";
+        }
+        else if (l.equals("french")) {
+            greeting = "Bonjour";
+        }
+        else if (l.equals("italian")) {
+            greeting = "Bonjourno";
+        }
+        else if (l.equals("spanish")) {
+            greeting = "Hola";
+        }
+        else if (l.equals("german")) {
+            greeting = "Hallo";
+        }
+
+        return greeting + " " + n;
     }
 
-    //handles requests for http://localhost:8080/hello/form
-    @GetMapping("form")
-    public String helloForm() {
-        return "<html>" +
-                "<body>" +
-                "<form action = '/hello' method = 'post'>" + // submit a request to /hello
-                "<input type = 'text' name = 'name' >" +
-                "<input type = 'submit' value = 'Greet Me!' >" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+    @RequestMapping(value="hello/{name}")
+    @ResponseBody
+    public String helloUrlSegment(@PathVariable String name) {
+        return "Hello " + name;
     }
 
+    @RequestMapping(value="goodbye")
+    @ResponseBody
+    public String goodbye() {
+        return "Goodbye!";
+    }
 }
+
--- controllers-and-routing/src/test/java/org/launchcode/hellospring/HelloSpringApplicationTests.java
+++ controllers-and-routing/src/test/java/org/launchcode/hellospring/HelloSpringApplicationTests.java
@@ -1,9 +1,7 @@
 package org.launchcode.hellospring;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class HelloSpringApplicationTests {
 
 	@Test
