c6c0e78511f0e494d2240cfcf790dea3f2c2d013
John Woolbright <jwoolbright23@gmail.com>
2023-06-28 14:21:21 -0500

    updated structure

--- controllers-and-routing/exercises/controllers-routing-exercises/src/test/java/org/launchcode/hellospring/HelloSpringApplicationTests.java
+++ controllers-and-routing/exercises/controllers-routing-exercises/src/test/java/org/launchcode/hellospring/HelloSpringApplicationTests.java
@@ -1,7 +1,9 @@
 package org.launchcode.hellospring;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class HelloSpringApplicationTests {
 
 	@Test
