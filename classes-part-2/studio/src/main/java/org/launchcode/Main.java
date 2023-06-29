0a190c06ae4c0f515ae83b473b3500b0d9913e18
Courtney Frey <courtneyb.frey@gmail.com>
2023-06-29 14:33:48 -0500

    adding starter code

--- classes-part-2/studio/src/main/java/org/launchcode/Main.java
+++ classes-part-2/studio/src/main/java/org/launchcode/Main.java
@@ -2,6 +2,7 @@ package org.launchcode;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Write Your Code Here!");
     }
 }
