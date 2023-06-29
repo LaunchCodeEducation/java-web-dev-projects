cabdd01847e925839a1408161336c9ced32a636e
Courtney Frey <courtneyb.frey@gmail.com>
2023-06-29 15:03:54 -0500

    Revert "adding starter code"
    This reverts commit 0a190c06ae4c0f515ae83b473b3500b0d9913e18.

--- classes-part-2/studio/src/main/java/org/launchcode/Main.java
+++ classes-part-2/studio/src/main/java/org/launchcode/Main.java
@@ -2,7 +2,6 @@ package org.launchcode;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Write Your Code Here!");
+        System.out.println("Hello world!");
     }
 }
