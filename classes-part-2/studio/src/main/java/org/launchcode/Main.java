974f486b7f94c2d351643723d9589ecffab51306
Courtney Frey <courtneyb.frey@gmail.com>
2023-06-29 15:15:35 -0500

    studio classes added:

--- classes-part-2/studio/src/main/java/org/launchcode/Main.java
+++ classes-part-2/studio/src/main/java/org/launchcode/Main.java
@@ -1,7 +1,8 @@
 package org.launchcode;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // write your code here
     }
 }
