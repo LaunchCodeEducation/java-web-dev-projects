2b8253aab3dab5e9abb99a1fa70e5997bdf74918
Courtney Frey <courtneyb.frey@gmail.com>
2023-07-10 14:56:20 -0500

    exercises starter code added

---
+++ interfaces/exercises/src/main/java/org/launchcode/Flavor.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Flavor extends Ingredient {
+
+
+    public Flavor(String aName, double aCost, ArrayList<String> someAllergens){
+        super (aName, aCost, someAllergens);
+    }
+
+
+}
