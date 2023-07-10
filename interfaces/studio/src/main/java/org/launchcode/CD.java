92588fdba42be6e8c1277090eb32300d9d858fae
Courtney Frey <courtneyb.frey@gmail.com>
2023-07-10 14:59:25 -0500

    studio added

---
+++ interfaces/studio/src/main/java/org/launchcode/CD.java
@@ -0,0 +1,8 @@
+public class CD {
+
+    // TODO: Implement your custom interface.
+
+    // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
+    //  need to be declared separately.
+
+}
