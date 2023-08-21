12e76569c443db100ca1d170c0a5d3a14a278b2f
Bjorlow <dimitritorres34@gmail.com>
2023-08-21 15:18:27 -0500

    Java exercises

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,16 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a radius: ");
+        double radius = input.nextDouble();
+
+        double area = Circle.getArea(radius);
+
+        System.out.println("The area of a circle of radius " + radius + " is: " + area);
+    }
+}
