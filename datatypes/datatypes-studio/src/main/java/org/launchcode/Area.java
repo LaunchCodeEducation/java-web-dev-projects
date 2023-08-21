faa1b45c58474b6f23ba01a5bc92b43f55a11e18
Dustin Dev <94726570+dustinFriesenDev@users.noreply.github.com>
2023-08-21 10:54:11 -0400

    Finished studio with bonus mission

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -5,13 +5,27 @@ import java.util.Scanner;
 
 
 public class Area {
-    public static void main(String[] arg){
+    public static void main(String[] arg) {
+        Double radius;
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a radius: ");
-        double radius = input.nextDouble();
-        double area = Circle.getArea(radius);
 
-        System.out.println(radius);
-        System.out.println("The area of a circle of a radius " + radius + "is: " + area);
+        try {
+            radius = input.nextDouble();
+            while (radius < 0) {
+                System.out.println("Invalid input: Please enter a positive radius: ");
+                radius = input.nextDouble();
+            }
+
+            Double area = Circle.getArea(radius);
+            System.out.println(radius);
+            System.out.println("The area of a circle of a radius " + radius + " is: " + area);
+
+        } catch (Exception e) {
+            System.out.println("Enter a numbered radius: Please start over");
+        }
+
+
+        input.close();
     }
 }
