f0b592b08fb5f6539abdf225cc4bad03f46efee6
katytapley <katy.tapley@gmail.com>
2023-08-22 13:13:55 -0500

    "Completed Class 2 Studio"

---
+++ control-flow-and-collections/studio/counting-characters/src/OccuranceOfChar.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+public class OccuranceOfChar {
+    static void characterCount(String inputString){
+
+        HashMap<Character, Integer> charCountMap = new HashMap<Character, Integer>();
+
+        char[] strArray = inputString.toCharArray();
+
+        for (char c : strArray) {
+            if (charCountMap.containsKey(c)){
+                charCountMap.put(c, charCountMap.get(c) + 1);
+            } else {
+                charCountMap.put(c, 1);
+            }
+        }
+
+        for (Map.Entry entry : charCountMap.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+}
