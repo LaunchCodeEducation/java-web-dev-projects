0d739b4178110b199ab5cbd01e4f26dcf97c2d67
may2fn <may2fn@umsystem.edu>
2023-08-24 19:55:16 -0500

    Completed studio 2

--- control-flow-and-collections/studio/counting-characters/src/main/java/org/example/Main.java
+++ control-flow-and-collections/studio/counting-characters/src/main/java/org/example/Main.java
@@ -1,19 +1,36 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String sentence = "If the product of two terms is zero then common sense says at least one of " +
+                "the two terms has to be zero to start with. So if you move all the terms over to one side, " +
+                "you can put the quadratics into a form that can be factored allowing that side of the equation " +
+                "to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter some text: ");
+        String userInput = input.nextLine();
+        userInput = userInput.replaceAll("[^a-zA-Z]", "");
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        HashMap<Character, Integer> characters = new HashMap<>();
+
+        for(int i = 0; i < userInput.length(); i++){
+            //System.out.println(sentence.charAt(i));
+            if(characters.containsKey(Character.toLowerCase(userInput.charAt(i)))){
+                characters.put(Character.toLowerCase(userInput.charAt(i)), characters.get(Character.toLowerCase(userInput.charAt(i))) + 1);
+            }
+            else{
+                characters.put(Character.toLowerCase(userInput.charAt(i)), 1);
+            }
+        }
+
+        for(Character key : characters.keySet()){
+            System.out.println(key + ": " + characters.get(key));
         }
     }
 }
