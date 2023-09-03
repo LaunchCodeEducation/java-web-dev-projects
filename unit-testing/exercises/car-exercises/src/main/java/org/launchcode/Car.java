5dfa0292cb3caef7825b9328d1cb02aa6a99028a
Sloane Wolter <sloanewolter@gmail.com>
2023-09-02 22:31:29 -0500

    Finished exercises for unit testing

--- unit-testing/exercises/car-exercises/src/main/java/org/launchcode/Car.java
+++ unit-testing/exercises/car-exercises/src/main/java/org/launchcode/Car.java
@@ -46,6 +46,9 @@ public class Car {
     }
 
     public void setGasTankLevel(double gasTankLevel) {
+        if (gasTankLevel > this.getGasTankSize()) {
+            throw new IllegalArgumentException("Can't exceed tank size.");
+        }
         this.gasTankLevel = gasTankLevel;
     }
 
@@ -82,6 +85,10 @@ public class Car {
         double gallonsUsed = milesAbleToTravel / this.milesPerGallon;
         this.gasTankLevel = this.gasTankLevel - gallonsUsed;
         this.odometer += milesAbleToTravel;
+    }
+
+    public void addGas(double gas) {
+        this.setGasTankLevel(gas + this.getGasTankLevel());
     }
 
 }
