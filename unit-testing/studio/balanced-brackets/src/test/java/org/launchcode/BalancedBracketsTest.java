ac818a88ddf5f03d967520366c22d6428643a732
katytapley <katy.tapley@gmail.com>
2023-09-06 19:46:17 -0500

    Finished studio

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -10,4 +10,60 @@ class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test
+    public void testSingleSet() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Launchcode]"));
+    }
+
+    @Test
+    public void testSingleSetTwo() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[code]"));
+    }
+
+    @Test
+    public void testSingleSetThree() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]Launchcode"));
+    }
+
+    @Test
+    public void testEmptyString() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void UnblanacedOne() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test
+    public void UnblanacedTwo() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+    @Test
+    public void UnblanacedThree() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void UnblanacedFour() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void testMultipleOpenClose() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+
+    @Test
+    public void testEndsWithExtraClose() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[[]]]]"));
+    }
+
 }
