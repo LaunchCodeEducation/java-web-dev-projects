12ce71c9d9cbf1ecf0b57ec5a5d35b801bb6d90b
rebeccay12 <beccayeager97@gmail.com>
2023-09-11 20:27:44 -0500

    Finished unit testing studio

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -1,13 +1,70 @@
 package org.launchcode;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BalancedBracketsTest {
+
     //TODO: add tests here
     @Test
     public void emptyTest() {
         assertEquals(true, true);
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void stringWithBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void stringWithNoBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("LaunchCode"));
+    }
+
+    @Test
+    public void stringWithTwoBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][]"));
+    }
+
+    @Test
+    public void stringWithOneBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void stringWithUnbalancedBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Launch[Code]"));
+    }
+
+    @Test
+    public void stringWithUnMatchedOpeningBrackets () {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch[Co[de"));
+    }
+
+    @Test
+    public void stringWithUnMatchedClosingBrackets () {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Co]de"));
+    }
+
+    @Test
+    public void stringWithIncorrectlyFacingBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void stringWithIncorrectlyFacingEndBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]]["));
+    }
+
+    @Test
+    public void stringWithManyBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]][[]]"));
     }
 }
