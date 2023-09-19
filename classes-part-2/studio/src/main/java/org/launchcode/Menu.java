4d93f31ce417e4b28e167e0a0400ff64b99f24ee
Sukanya <sukanya.s1793@gmail.com>
2023-09-19 18:44:42 -0500

    resturant menu done

--- classes-part-2/studio/src/main/java/org/launchcode/Menu.java
+++ classes-part-2/studio/src/main/java/org/launchcode/Menu.java
@@ -4,29 +4,40 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Menu {
-    private Date lastUpdated;
-    private ArrayList<MenuItem> items;
 
-    public Menu(Date d, ArrayList<MenuItem> i) {
-        this.lastUpdated = d;
-        this.items = i;
+    private ArrayList<MenuItem> menuItems = new ArrayList<>();
+
+    public ArrayList<MenuItem> getMenuItems() {
+        return menuItems;
     }
-
-    public void setLastUpdated(Date lastUpdated) {
-        this.lastUpdated = lastUpdated;
+    public String toString() {
+        StringBuilder appetizers = new StringBuilder();
+        for (MenuItem item : menuItems) {
+            if (item.getCategory().equals("appetizer")) {
+                appetizers.append("\n" + item.toString() + "\n");
+            }
+        }
+        StringBuilder mainCourses = new StringBuilder();
+        for (MenuItem item : menuItems) {
+            if (item.getCategory().equals("main course")) {
+                mainCourses.append("\n" + item.toString() + "\n");
+            }
+        }
+        StringBuilder desserts = new StringBuilder();
+        for (MenuItem item : menuItems) {
+            if (item.getCategory().equals("dessert")) {
+                desserts.append("\n" + item.toString() + "\n");
+            }
+        }
+        return "\nTONY'S PIZZA MENU\n" +
+                "APPETIZERS" + appetizers.toString() + "\n" +
+                "MAIN COURSES" + mainCourses.toString() + "\n" +
+                "DESSERTS" + desserts.toString() + "\n";
     }
-
-    public void setItems(ArrayList<MenuItem> items) {
-        this.items = items;
+    void removeItem(MenuItem item) {
+        menuItems.remove(item);
     }
-
-    public Date getLastUpdated() {
-        return lastUpdated;
-    }
-
-    public ArrayList<MenuItem> getItems() {
-        return items;
+    void addItem(MenuItem newItem) {
+        menuItems.add(newItem);
     }
 }
-
-
--- classes-part-2/studio/src/main/java/org/launchcode/MenuItem.java
+++ classes-part-2/studio/src/main/java/org/launchcode/MenuItem.java
@@ -1,32 +1,62 @@
 package org.launchcode;
-
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 public class MenuItem {
+    private String name;
     private double price;
     private String description;
     private String category;
-    private boolean isNew;
+    private final LocalDate dateAdded;
 
-    public MenuItem(double p, String d, String c, boolean iN) {
-        this.price = p;
-        this.description = d;
-        this.category = c;
-        this.isNew = iN;
+    public MenuItem(String name, String description, double price, String category) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.category = category;
+        this.dateAdded = LocalDate.now();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
     }
 
     public void setPrice(double price) {
         this.price = price;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
     }
 
     public void setCategory(String category) {
         this.category = category;
     }
 
-    public void setNew(boolean aNew) {
-        isNew = aNew;
+    public LocalDate getDateAdded() {
+        return dateAdded;
     }
+    public String toString() {
+//String newText = isNew() ? " - NEW!" : "";
+        return name +  '\n' +
+                description + " | $" + price;
+    }
+
+
 }
 
