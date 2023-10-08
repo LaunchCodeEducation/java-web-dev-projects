32dc16ffd30b1d32de4def1474099fa39a889728
drbrgo <d.b.goga@gmail.com>
2023-10-08 19:44:11 -0400

    completed exercises for validation in codingevents

--- spring/codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ spring/codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,9 +1,11 @@
 package org.launchcode.codingevents.controllers;
 
+import jakarta.validation.Valid;
 import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -35,7 +37,9 @@ public class EventController {
     }
 
     @GetMapping("create")
-    public String displayCreateEventForm() {
+    public String displayCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
+        model.addAttribute("event", new Event());
         return "events/create";
     }
 
@@ -47,7 +51,14 @@ public class EventController {
 //    }
 
     @PostMapping("create")
-    public String createEvent(@ModelAttribute Event newEvent) {
+    public String createEvent(@ModelAttribute @Valid Event newEvent, Errors errors, Model model) {
+        if(errors.hasErrors()){
+            model.addAttribute("title", "Create Event");
+//            model.addAttribute("errorMsg", "Bad data!");
+            return "events/create";
+        }
+//        if(Integer.parseInt(newEvent.getZipcode())
+
         EventData.add(newEvent);
         return "redirect:/events";
     }
--- spring/codingevents/src/main/java/org/launchcode/codingevents/models/Event.java
+++ spring/codingevents/src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,19 +1,91 @@
 package org.launchcode.codingevents.models;
 
+import jakarta.validation.constraints.*;
+
 import java.util.Objects;
 
 public class Event {
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters.")
+    @NotBlank(message = "Name is required.")
     private String name;
+    @Size(max = 500, message = "Description is too long.")
+    @NotBlank(message = "Description is required.")
     private String description;
+    @Email(message = "Invalid email. Try again.")
+    @NotBlank(message = "Email is required.")
+    private String contactEmail;
+
+    @NotBlank(message = "Event street address is required.")
+    private String address;
+
+    private Boolean registrationRequired;
+
+    @Positive(message = "Number of attendees must be greater than zero.")
+    private int numAttendees;
+
+@NotBlank(message="Event zipcode required.")
+@Size(min=5, max=5, message="Must enter 5-digit zipcode")
+    private String zipcode;
+
     private int id;
     private static int nextId=1;
 
-    public Event(String name, String description) {
+    public Event(String name, String description, String contactEmail, String address, String zipcode, Boolean registrationRequired, int numAttendees) {
+        this();
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
+        this.address = address;
+        this.zipcode = zipcode;
+        this.registrationRequired = registrationRequired;
+        this.numAttendees = numAttendees;
+
+    }
+    public Event() {
         this.id=nextId;
         nextId++;
     }
+
+    public int getNumAttendees() {
+        return numAttendees;
+    }
+
+    public void setNumAttendees(int numAttendees) {
+        this.numAttendees = numAttendees;
+    }
+
+    public Boolean getRegistrationRequired() {
+        return registrationRequired;
+    }
+
+    public void setRegistrationRequired(Boolean registrationRequired) {
+        this.registrationRequired = registrationRequired;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
     public int getId() {
         return id;
     }
