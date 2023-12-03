56bdd9f585d2846ab47f859566d7131284d59265
megha saxena <meghasaxena9@gmail.com>
2023-12-03 17:57:49 -0600

    message

--- datatypes/datatypes-studio/src/main/Java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/Java/org/launchcode/Area.java
@@ -6,27 +6,43 @@ import java.util.Scanner;
 
 public class Area {
     public static void main(String[] args) {
-        Double radius;
+        Double radius = null;
         double pi = 3.14;
         Double area;
         Scanner input;
 
         input = new Scanner(System.in);
         System.out.println("Enter a radius: ");
+      //  input.next();
         radius = input.nextDouble();
+     /*
+        while (input.hasNext()) {
 
-        if (radius.isNaN()) {
-            System.out.println("Invalid Input");
+            if (input.hasNextDouble()) {
+         //       radius = input.nextDouble();
+                if (input.nextDouble() < 0) {
+                    System.out.println("Invalid Input try again");
+                    input.next();
+                } else {
+
+                    radius = input.nextDouble();
+                    //      input.next();
+                          input.close();
+                }
+            }
+                else {
+                    System.out.println("Invalid Input try again");
+                    input.next();
+//                    input.close();
+                }
+            }
+
             input.close();
-        }
 
-        if (radius < 0) {
-            System.out.println("Enter a radius: ");
-            radius = input.nextDouble();
-        }
-        input.close();
 
       //  area = (pi) * radius * radius;
+
+      */
         area = Circle.getArea(radius);
         System.out.println("The area of a circle of radius " + radius + " is: " + area);
     }
