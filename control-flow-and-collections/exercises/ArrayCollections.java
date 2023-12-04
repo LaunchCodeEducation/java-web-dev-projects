6e902b224873271dae4c4430b0ffe3fe44b65861
megha saxena <meghasaxena9@gmail.com>
2023-12-04 15:19:30 -0600

    message

---
+++ control-flow-and-collections/exercises/ArrayCollections.java
@@ -0,0 +1,15 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+        public class ArrayCollections {
+            public static void main(String[] args) {
+                int[] integerArray = {1, 1, 2, 3, 5, 8};
+            }
+            String phrase = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+            String[] words = phrase.split(" ");
+System.out.println(Arrays.toString(words));
+}
+   System.out.println(Arrays.toString(arrayName));
+           String[] sentences = phrase.split("\\.");
+           System.out.println(Arrays.toString(sentences));
