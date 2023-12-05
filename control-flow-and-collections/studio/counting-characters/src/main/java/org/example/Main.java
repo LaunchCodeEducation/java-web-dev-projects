80997d6dfcae782e1031b517bed74c5e52c2e87e
megha saxena <meghasaxena9@gmail.com>
2023-12-04 22:12:11 -0600

    message

--- control-flow-and-collections/studio/counting-characters/src/main/java/org/example/Main.java
+++ control-flow-and-collections/studio/counting-characters/src/main/java/org/example/Main.java
@@ -1,9 +1,24 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+
+public class CountingCharacters {
+
     public static void main(String[] args) {
+
+        String myString = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. " +
+                "So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero." +
+                " Once you’ve done that, it’s pretty straightforward from there.";
+        char[] charactersInString = myString.toCharArray();
+
+
+
+
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
