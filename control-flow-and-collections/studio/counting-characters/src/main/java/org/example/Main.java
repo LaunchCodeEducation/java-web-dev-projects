e912bdce26f88865108963d179938d661aa8b52e
megha saxena <meghasaxena9@gmail.com>
2023-12-06 17:25:20 -0600

    message

--- control-flow-and-collections/studio/counting-characters/src/main/java/org/example/Main.java
+++ control-flow-and-collections/studio/counting-characters/src/main/java/org/example/Main.java
@@ -1,34 +1,37 @@
-package org.example;
-
+char[] charactersInString = myString.toUpperCase().toCharArray();
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-
 public class CountingCharacters {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
-        String myString = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. " +
-                "So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero." +
-                " Once you’ve done that, it’s pretty straightforward from there.";
-        char[] charactersInString = myString.toCharArray();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter $$ if you want a file or enter a string to count: ");
 
 
+        String input = scanner.nextLine();
+        scanner.close();
 
+        if (input == "$$"){
+            Scanner scans = new Scanner(filePath);
+        } else {
+            input = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. " +
+                    "So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero." +
+                    " Once you've done that, it's pretty straightforward from there.";
 
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+            String myString = input
+        }
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        HashMap<Character, Integer> chars = new HashMap<>();
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        for(int i = 0; i < charactersInString.length; i++){
+            chars.put(charactersInString[i], chars.getOrDefault(charactersInString[i], 0) + 1);
+        }
+
+        for (Map.Entry<Character, Integer> letter : chars.entrySet()) {
+            System.out.println(letter.getKey() + " :" + letter.getValue());
         }
     }
 }
