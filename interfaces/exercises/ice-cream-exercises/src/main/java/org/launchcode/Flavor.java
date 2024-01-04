64f0da2b77cfa6c6a8060d6b68999ac80ac1fb12
megha saxena <meghasaxena9@gmail.com>
2024-01-04 15:09:47 -0600

    message

--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Flavor.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Flavor.java
@@ -1,9 +1,14 @@
 package org.launchcode;
 
 import java.util.ArrayList;
-
+import java.util.Comparator;
 public class Flavor extends Ingredient {
-
+    public class FlavorComparator implements Comparator<Flavor> {
+        @Override
+        public int compare(Flavor flavor1, Flavor flavor2) {
+            return flavor1.getName().compareTo(flavor2.getName());
+        }
+    }
 
     public Flavor(String aName, double aCost, ArrayList<String> someAllergens){
         super (aName, aCost, someAllergens);
